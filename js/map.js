8f30a18ea9356a0a5343b75ecac270305536c185
ishancoder <isaxena171@gmail.com>
2015-11-12 08:16:18 +0530

    added a map refreshing function

--- js/map.js
+++ js/map.js
@@ -1,10 +1,15 @@
 var init = function(){
-	var mapProp = {
+	this.mapProp = {
 		center: new google.maps.LatLng(0,0),
 		zoom: 2,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
-	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	this.map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
 
 google.maps.event.addDomListener(window,'load',init);
+
+init.refreshMap = function(lat,lng) {
+	map.panTo(new google.maps.LatLng(lat,lng));
+}
+
